f9feac5678ea53e14e3dbf24cf6e026fea5df752
Sara <srancati0@gmail.com>
2019-10-15 19:44:06 +0100

    steps 1-7

---
+++ spec/dockingstation_spec.rb
@@ -0,0 +1,13 @@
+require 'docking_station'
+
+describe DockingStation do
+  it { should respond_to(release_bike) }
+ end
+
+=begin
+describe DockingStation do
+  it "should respond" do
+  subject.should respond_to(release_bike)
+end
+end
+=end
