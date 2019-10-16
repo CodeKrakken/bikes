2619c7b9f186d8a089c0fe6351317efadfcb8a09
fahus <faduma.h@yahoo.com>
2019-10-16 17:25:31 +0100

    step 11/12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,26 @@
-class DockingStation
-  def release_bike
 
+
+class DockingStation
+  attr_reader :bike
+
+
+  def working?
   end
+
+  def release_bike
+    Bike.new
+  end
+
+  def dock(bike)
+    @bike = bike
+ end
+end
+
+class Bike
+  def working?
+    true
+end
+
+  def release_bike
+  @bike
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,13 +1,21 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { should respond_to(release_bike) }
- end
+  it { should respond_to(:release_bike) }
+  it { should respond_to :working? }
+  it { should respond_to(:dock).with(1).argument }
+  it { should respond_to (:bike) }
 
-=begin
-describe DockingStation do
-  it "should respond" do
-  subject.should respond_to(release_bike)
+  describe '#release_bike' do
+    it 'releases a bike'
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq bike
 end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 end
-=end
