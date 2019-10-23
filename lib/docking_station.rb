713b8840381cadfb5245d31addb574ecba656b7a
CodeKrakken <newholm1@hotmail.com>
2019-10-23 14:02:29 +0000

    cleaning up workspace

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,26 +2,27 @@
 
 class DockingStation
   attr_reader :bike
-
-
+  
+  def initialize
+    @bikes = []
+  end  
+  
   def working?
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bike
-    @bike = bike
+    fail 'Docking station full' if @bikes.count >= 20
+    @bikes << bike
   end
   
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if @bikes.empty?
+    @bikes.pop
   end
 end
 
 class Bike
   def working?
-    true
+    return true
   end
-
-  
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -20,15 +20,9 @@ describe DockingStation do
       expect(subject.release_bike).to eq bike
     end
 
-    it 'returns docked bikes' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.bike).to eq bike
-    end
-    
   describe '#dock' do
     it 'raises an error when full' do
-      subject.dock(Bike.new)
+      20.times { subject.dock Bike.new }
       expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
